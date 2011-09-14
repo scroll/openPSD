d3ca901f2bdb6c262ad6eed9cacfeaef6b0b0f04
scroll lock <scroll_lock@abv.bg>
2011-09-14 10:48:03 -0400

    wasnt able to load properly on some machines..weird

--- openPSD.py
+++ openPSD.py
@@ -8,26 +8,32 @@ nodeId = OM.MTypeId(0x101121)
 class openPSD(OMMPx.MPxNode):
     # constructor
     def __init__(self):
+        OMMPx.MPxNode.__init__(self)
         print '__init__'
-        OMMPx.MPxConstraint.__init__(self)
 
     def compute(self, plug, dataBlock):
         print ' compute '
 
 
 def nodeCreator():
-        return OMMPx.asMPxPtr(openPSD())
+    return OMMPx.asMPxPtr(openPSD())
 
 def nodeInit():
-        nAttr = OM.MFnNumericAttribute()
-        uAttr = OM.MFnUnitAttribute()
+    nAttr = OM.MFnNumericAttribute()
+    openPSD.aTest = nAttr.create( "test", "tst" ,OM.MFnNumericData.kFloat)
+    nAttr.setDefault(0.5)
+    nAttr.setKeyable(True)
+    openPSD.addAttribute(openPSD.aTest)
 
 
 # initialize the plugin
 def initializePlugin(mobject):
+    print 'Initializing plugin'
     mplugin = OMMPx.MFnPlugin(mobject,'Marin&Slavi', '0.1', 'Any')
+    print 'Before try...'
     try:
-        mplugin.registerNode(pluginName, nodeId, nodeCreator ,nodeInit, OMMPx.MPxNode)
+        mplugin.registerNode(pluginName, nodeId, nodeCreator ,nodeInit)
+
     except:
         raise Exception, 'Unable to register node: %s' %pluginName
 
